435a755ab43a4277ed37adf455f2ccab84a86063
AleksanderAleksandrovich <aleksveles228@gmail.com>
2023-11-04 19:44:50 +0300

    фикс адаптива слайдера

--- src/components/Slider/SliderImg.tsx
+++ src/components/Slider/SliderImg.tsx
@@ -7,9 +7,13 @@ type Props = {
 
 const SliderImg = ({ text, img }: Props) => {
   return (
-    <div className="container_slider_full">
+    <div className="container_slider_full min-h-[13.7rem] sm:min-h-[20.3rem]">
       <div className="image-container_slider_full">
-        <img src={img} alt={img} className="image_slider" />
+        <img
+          src={img}
+          alt={img}
+          className="image_slider min-h-[13.7rem] sm:min-h-[20.3rem]"
+        />
         <div className="overlay_slider_full">
           <div className="text_slider_full flex items-center justify-center font-bergamasco text-[3rem] md:text-[6rem] xl:text-[6.9rem]">
             {text}
