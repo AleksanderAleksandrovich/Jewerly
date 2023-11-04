a65cd4678bf065a0c69ebd1076944cb7fa89d08e
AleksanderAleksandrovich <aleksveles228@gmail.com>
2023-11-04 20:20:16 +0300

    рефакторинг

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -1,7 +1,7 @@
 import { BreadCrumbs } from '@/components/BreadCrambs'
 import Slider from '@/components/Slider'
 import { Outlet } from 'react-router-dom'
-type Props = {}
+
 
 const Catalog = () => {
   return (
